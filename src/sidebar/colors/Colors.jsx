a320726c6091fcd3cb99b7a2e3e344dfc2bea83c
Md. Mezbaul Islam ZIon <mezbaul15-3496@diu.edu.bd>
2024-10-18 20:25:26 +0600

    Refactor: Update CSS styles and add Sidebar component

--- src/sidebar/colors/Colors.jsx
+++ src/sidebar/colors/Colors.jsx
@@ -1,6 +1,30 @@
 const Colors = () => {
   return (
-    <div>Colors</div>
+    <div>
+      <h2 className="sidebar-title color-title">
+        Colors
+      </h2>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" id="" />
+          <span className="checkmark"></span>All
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" id="" />
+          <span className="checkmark"></span>Black
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" id="" />
+          <span className="checkmark"></span>White
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" id="" />
+          <span className="checkmark"></span>Red
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" id="" />
+          <span className="checkmark"></span>Blue
+        </label>
+    </div>
   )
 }
 export default Colors
--- src/sidebar/price/Price.jsx
+++ src/sidebar/price/Price.jsx
@@ -1,6 +1,32 @@
+import './Price.css';
+
 const Price = () => {
   return (
-    <div>Price</div>
+    <div className="ml">
+      <h2 className="sidebar-title price-title">
+        Price
+      </h2>
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test" id="" />
+        <span className="checkmark"></span>All
+      </label>
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test2" id="" />
+        <span className="checkmark"></span>0 - $50
+      </label>
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test3" id="" />
+        <span className="checkmark"></span>$50 - $100
+      </label>
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test4" id="" />
+        <span className="checkmark"></span>$100 - $150
+      </label>
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test5" id="" />
+        <span className="checkmark"></span>$150 - $200
+      </label>
+    </div>
   )
 }
 export default Price
